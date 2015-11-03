8b22ccd2b7aa97d1dec7c68e9bf713239ebc7b4d
Abhishek Sharma <abhisheks1712@gmail.com>
2015-11-03 23:46:11 +0530

    Part 1 of homework 0 is over.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,29 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  s = 0
+  arr.each do |num|
+    s += num
+  end
+  s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  s = 0
+  if arr.length == 0
+    s
+  elsif arr.length ==1
+    s = arr[0]
+  else
+    arr1 = arr.sort
+    s = arr1[arr.length - 1] + arr1[arr.length - 2]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr1 = arr.combination(2).to_a
+  arr1.map!{ |element| sum(element) }
+  arr1.include?(n)
 end
 
 # Part 2
